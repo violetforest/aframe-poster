a051d69f6c0a0e1f4c5796d51eb27849588d32c8
Violet Forest <info@violetforest.com>
2018-06-10 00:39:37 -0400

    clamp dots

--- shaders/simple-shader.js
+++ shaders/simple-shader.js
@@ -1,17 +1,22 @@
 AFRAME.registerShader('simple', {
   schema: {
     timeMsec: {type:'time', is:'uniform'},
-    resolution: {type: 'vec2', is: 'uniform', default: {x:500, y:500}}
+    resolution: {type: 'vec2', is: 'uniform', default: {x:500, y:500}},
+    displacement: {type: 'float', is: 'attribute'},
+    amplitude: {type: 'float', default: 0}
   },
   vertexShader: `
 
     varying vec2 vUv;
     varying vec3 vNormal;
+    attribute float displacement;
+    uniform float amplitude;
 
     void main() {
       vUv = uv;
       vNormal = normal;
-      gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+      vec3 newPosition = position + normal * vec3(displacement * amplitude);
+      gl_Position = projectionMatrix * modelViewMatrix * vec4(newPosition, 1.0 );
     }
 
 `,
@@ -23,16 +28,17 @@ AFRAME.registerShader('simple', {
     uniform vec2 resolution;
 
     void main() {
-      vec3 light = vec3(0.5, 0.2, 1.0);
+      vec3 light = vec3(0.5, -1.0, -1.0);
       light = normalize(light);
       float dot = dot(vNormal, light);
       float dotProduct = max(0.0, dot);
+      float dotClamped = clamp(dotProduct, 0.0, dot);
       vec2 pos = gl_FragCoord.xy/resolution;
       float r = sin(pos.y * timeMsec);
       float g = 0.572 * cos(pos.x * timeMsec);
       float b = 0.164 + sin(pos.x * 3.252) + sin(pos.y + 5.348) * r;
       g = fract(500.0 - sin(b));
-  	  gl_FragColor = vec4(r, g, dotProduct, 3.976);
+  	  gl_FragColor = vec4(r, dotClamped, b, 3.976);
     }
 
 `
