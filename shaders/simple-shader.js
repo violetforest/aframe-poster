17ed5ba1651bfc088405d21c974feaae92e13e64
Violet Forest <info@violetforest.com>
2018-06-10 00:02:31 -0400

    dot shader

--- shaders/simple-shader.js
+++ shaders/simple-shader.js
@@ -11,11 +11,6 @@ AFRAME.registerShader('simple', {
     void main() {
       vUv = uv;
       vNormal = normal;
-
-      vec3 light = vec3(0.5, 0.2, 1.0);
-      light = normalize(light);
-
-      float dProd = max(0.0, dot(vNormal, light));
       gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
     }
 
@@ -23,16 +18,21 @@ AFRAME.registerShader('simple', {
   fragmentShader: `
 
     varying vec2 vUv;
+    varying vec3 vNormal;
     uniform float timeMsec;
     uniform vec2 resolution;
 
     void main() {
-        vec2 pos = gl_FragCoord.xy/resolution;
-        float r = sin(pos.y * timeMsec);
-        float g = 0.572 * cos(pos.x * timeMsec);
-        float b = 0.164 + sin(pos.x * 3.252) + sin(pos.y + 5.348) * r;
-        g = fract(500.0 - sin(b));
-    	gl_FragColor = vec4(r, g, b, 3.976);
+      vec3 light = vec3(0.5, 0.2, 1.0);
+      light = normalize(light);
+      float dot = dot(vNormal, light);
+      float dotProduct = max(0.0, dot);
+      vec2 pos = gl_FragCoord.xy/resolution;
+      float r = sin(pos.y * timeMsec);
+      float g = 0.572 * cos(pos.x * timeMsec);
+      float b = 0.164 + sin(pos.x * 3.252) + sin(pos.y + 5.348) * r;
+      g = fract(500.0 - sin(b));
+  	  gl_FragColor = vec4(r, g, dotProduct, 3.976);
     }
 
 `
